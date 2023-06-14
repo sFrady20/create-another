07947659cd121dfe42a5e91e728f633d56a438e4
Steven Frady <sfrady20@gmail.com>
2023-06-14 00:07:58 -0400

    cleanup

---
+++ packages/one/README.md
@@ -0,0 +1,44 @@
+## create-another-one
+
+The following documentation provides an overview of the provided script and its functionality. The script is designed to act as a template management system using the npm "create" prefix. It allows users to create a new project based on a template by running `npm create-another-one` or `yarn create another-one` and specifying the options and arguments.
+
+### Usage
+
+To use the script, follow these steps:
+
+1. Open your terminal or command prompt.
+2. Run the script using either of the following commands:
+   - `npm create-another-one [directory]` (replace `[directory]` with the desired project slug)
+   - `yarn create another-one [directory]` (replace `[directory]` with the desired project slug)
+3. Optionally, provide the `-t` or `--template` flag followed by the desired template value.
+4. Follow the prompts and provide the required information.
+5. The script will download the specified template into a new directory and initialize the Git history for the project.
+
+### Command-Line Options
+
+The script supports the following command-line options:
+
+- `-t, --template <template>`: Specifies the template to use for the project. Available options are:
+  - `web`: Website template
+  - `app`: Mobile App template
+  - `desktop`: Desktop App template
+  - `lib`: Library template
+  - `kiosk`: Kiosk template
+
+### Example Usage
+
+```shell
+# Create a new project with a specific name and template
+$ npm create-another-one my-project -t web
+
+# Create a new project with a specific name and template (using Yarn)
+$ yarn create another-one my-project --template app
+
+# Create a new project with default name and prompt for template selection
+$ npm create-another-one
+
+# Create a new project with default name and prompt for template selection (using Yarn)
+$ yarn create another-one
+```
+
+These are just examples, and you can adjust the commands and options as needed based on your requirements.
--- packages/one/src/index.ts
+++ packages/one/src/index.ts
@@ -1,49 +1,91 @@
 import inquirer from "inquirer";
 import { simpleGit } from "simple-git";
-import { mkdir, rm } from "fs/promises";
+import { mkdir, rm, readdir } from "fs/promises";
 import path from "path";
 import { existsSync } from "fs";
 import { program } from "commander";
+import colors from "colors";
+
+console.log(colors.green(colors.bold("Another one ☝️")));
+
+async function isEmpty(path: string) {
+  return (await readdir(path)).length === 0;
+}
+
+async function emptyDir(dir: string) {
+  if (!existsSync(dir)) return;
+  const files = await readdir(dir);
+  for (const file of files)
+    await rm(path.resolve(dir, file), { recursive: true, force: true });
+}
 
 program
   .argument("[directory]", "Project slug")
   .option("-t, --template <template>", "Template")
   .parse(process.argv);
 
-(async () => {
+async function init() {
   //find answers
   const answers = {
-    name:
+    name: (
       program.args[0] ||
       (
         await inquirer.prompt({
           name: "name",
-          message: "What's the name of your project?",
+          message: "Project name:",
           type: "input",
-          validate: (x) => (/^[a-z0-9\-]+$/gi.test(x) ? true : "invalid"),
+          validate: (x) =>
+            /^[a-z0-9\-]+$/gi.test(x)
+              ? true
+              : `Only "-" and alphanumeric characters allowed`,
         })
-      ).name,
+      ).name
+    ).toLowerCase(),
+
     template:
       program.opts().template ||
       (
         await inquirer.prompt({
           name: "template",
-          message: "What are you making?",
+          message: "Select a framework:",
           type: "list",
           choices: [
-            { name: "Website", value: "web" },
-            { name: "Mobile App", value: "app" },
-            { name: "Desktop App", value: "desktop" },
-            { name: "Library", value: "lib" },
-            { name: "Kiosk", value: "kiosk" },
+            { name: colors.white("Website"), value: "web" },
+            { name: colors.green("Mobile App"), value: "app" },
+            { name: colors.blue("Desktop App"), value: "desktop" },
+            { name: colors.cyan("Kiosk"), value: "kiosk" },
+            { name: colors.yellow("Library"), value: "lib" },
           ],
         })
       ).template,
   };
 
   //ensure path
-  const projectPath = path.join(process.cwd(), `./${answers.name}`);
+  const projectPath = path.join(
+    process.cwd(),
+    `./${answers.name.toLowerCase()}`
+  );
   if (!existsSync(projectPath)) await mkdir(projectPath);
+
+  //make sure directory is
+  if (!(await isEmpty(projectPath))) {
+    if (
+      (
+        await inquirer.prompt({
+          name: "remove",
+          message: `Target directory ${colors.bold(
+            `"${answers.name}"`
+          )} is not empty. Remove existing files and continue?`,
+          type: "confirm",
+        })
+      ).remove
+    )
+      await emptyDir(projectPath);
+    else {
+      console.log(colors.red("❌ Cancelled"));
+      return;
+    }
+  }
 
   //clone template
   const git = simpleGit(projectPath);
@@ -59,4 +101,9 @@ program
 
   //reinitialize git
   await git.init();
-})();
+
+  console.log(`Project created at ${colors.bold(`"${projectPath}"`)}`);
+  console.log(colors.bgBlack(colors.blue(`cd ./${answers.name}; yarn start`)));
+}
+
+init();
